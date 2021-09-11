9eb599edf67a2810f08e0bca9bd186ee9b8d410c
Chase-V <38985553+Chase-V@users.noreply.github.com>
2021-09-12 01:19:07 +0300

    Коммит перед попыткой добавления прогноза погоды

--- app/src/main/java/com/tashev/gbweatherfromya/repository/weatherLoaderAndDTO/WeatherDTO.kt
+++ app/src/main/java/com/tashev/gbweatherfromya/repository/weatherLoaderAndDTO/WeatherDTO.kt
@@ -1,5 +1,3 @@
 package com.tashev.gbweatherfromya.repository.weatherLoaderAndDTO
 
-import com.tashev.gbweatherfromya.repository.weatherLoaderAndDTO.FactDTO
-
 data class WeatherDTO(val fact: FactDTO)
